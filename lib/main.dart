d00ecbac3edd1671a8d24541be92cc1f63225976
Ilya <ilya.gordinov@aiesec.net>
2022-09-13 22:11:05 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'GB_Homework'),
     );
@@ -40,97 +39,132 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   var item = 0;
   final controller = CupertinoTabController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-         title: Text(widget.title),
+          title: Text(widget.title),
         ),
-      body: Container(
-
-        child: CupertinoTabScaffold(
-         controller: controller,
+        body: CupertinoTabScaffold(
+          controller: controller,
           tabBuilder: (BuildContext context, int index) {
-           switch (index) {
-             case 0: return Container(
-               color: Colors.blue,
-               child: ListView(
-                 physics: const BouncingScrollPhysics(),
-                 padding: const EdgeInsets.all(45),
-                 children: list.map((e) => Text(e, style: const TextStyle(fontSize: 100))).toList(),
-                 ),
-             );
-             case 1: return Container(
-               color: Colors.green,
-               child: ListView.builder(
-                 physics: const BouncingScrollPhysics(),
-                 padding: const EdgeInsets.all(45),
-                 itemCount: list.length,
-                 itemBuilder: (BuildContext context, int index) => Text(list[index], style: const TextStyle(fontSize: 100))
-               ),
-               );
-             case 2: return Container(
-               color: Colors.greenAccent,
-               alignment: FractionalOffset.center,
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: <Widget>[
-                   Padding(
-                     padding: const EdgeInsets.all(8.0),
-                     child: Icon(
-                       Icons.lightbulb_outline,
-                       color: _lightIsOn ? Colors.yellow.shade600 : Colors.black,
-                       size: 200,
-                     ),
-                   ),
-                   GestureDetector(
-                     onDoubleTap: () {
-                       setState(() {
-                         _lightIsOn = !_lightIsOn;
-                       });
-                     },
-                     child: Container(
-                       color: Colors.yellow.shade600,
-                       padding: const EdgeInsets.all(8),
-                       child: Text(style: const TextStyle(fontSize: 48),_lightIsOn ? 'TURN LIGHT OFF' : 'TURN LIGHT ON'),
-                     ),
-                   ),
-                 ],
-               ),
-             );
-             case 3: return Container(
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                 Padding(
-                   padding: const EdgeInsets.all(0.0),
-                   child: Image.network(_isImg1 ? 'http://placekitten.com/g/200/300' : 'http://placekitten.com/200/300', fit: BoxFit.cover),
-                   ),
-                 GestureDetector(
-                   onDoubleTap: () {
-                     setState(() {
-                       _isImg1 = !_isImg1;
-                     });
-                   },
+            switch (index) {
+              case 0:
+                return Container(
+                  color: Colors.blue,
+                  child: ListView(
+                    physics: const BouncingScrollPhysics(),
+                    padding: const EdgeInsets.all(45),
+                    children: list
+                        .map((e) =>
+                        Text(e, style: const TextStyle(fontSize: 100)))
+                        .toList(),
                   ),
-                 ],
-               ),
-             );
+                );
+              case 1:
+                return Container(
+                  color: Colors.green,
+                  child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      padding: const EdgeInsets.all(45),
+                      itemCount: list.length,
+                      itemBuilder: (BuildContext context, int index) => Text(
+                          list[index],
+                          style: const TextStyle(fontSize: 100))),
+                );
+              case 2:
+                return Container(
+                  color: Colors.greenAccent,
+                  alignment: FractionalOffset.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.lightbulb_outline,
+                          color: _lightIsOn
+                              ? Colors.yellow.shade600
+                              : Colors.black,
+                          size: 200,
+                        ),
+                      ),
+                      GestureDetector(
+                        onDoubleTap: () {
+                          setState(() {
+                            _lightIsOn = !_lightIsOn;
+                          });
+                        },
+                        child: Container(
+                          color: Colors.yellow.shade600,
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                              style: const TextStyle(fontSize: 48),
+                              _lightIsOn ? 'TURN LIGHT OFF' : 'TURN LIGHT ON'),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              case 3:
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: GestureDetector(
+                        onDoubleTap: () {
+                          setState(() {
+                            _isImg1 = !_isImg1;
+                          });
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.network(
+                            _isImg1
+                                ? 'http://placekitten.com/g/200/300'
+                                : 'http://placekitten.com/200/300',
+                            fit: BoxFit.cover,
+                            height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width,
+
+                          ),
+                        ),
+                      ),
+                    ),
+
+                  ],
+                );
             }
             return Container();
           },
           tabBar: CupertinoTabBar(
             backgroundColor: Colors.blue,
             items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.add, color: Colors.white,)),
-              BottomNavigationBarItem(icon: Icon(Icons.remove, color: Colors.white,)),
-              BottomNavigationBarItem(icon: Icon(Icons.account_balance, color: Colors.white,)),
-              BottomNavigationBarItem(icon: Icon(Icons.ad_units, color: Colors.white,)),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  )),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.remove,
+                    color: Colors.white,
+                  )),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.account_balance,
+                    color: Colors.white,
+                  )),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.ad_units,
+                    color: Colors.white,
+                  )),
             ],
           ),
         ),
-      ),
       ),
     );
   }
